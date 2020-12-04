7b56966ae70b890e1f70f10706b69b0d9cd5ae8c
William <wjha.1998@hotmail.com>
2020-12-04 01:47:12 -0500

    my portfolio finished

--- js/main.js
+++ js/main.js
@@ -9,3 +9,56 @@ openMenu.addEventListener("click", (e) => {
 closeMenu.addEventListener("click", (e) => {
     navMovil.style.left = "-100%";
 })
+
+$(function() {
+    'use strict'
+
+
+    $('.main-body section:first').show();
+    $('.navbar-desktop nav a:first').addClass('active');
+
+    $('.navbar-desktop nav a').on('click', mostrarTabs);
+
+    function mostrarTabs() {
+        $('.navbar-desktop nav a').removeClass('active');
+        $(this).addClass('active');
+        var enlace = $(this).attr('href');
+        $('.main-body section').hide();
+        $(enlace).fadeIn(1000);
+        return false;
+    }
+});
+
+$(function() {
+    'use strict'
+
+
+    $('.main-body section:first').show();
+    $('.navbar-movil nav a:first').addClass('active');
+
+    $('.navbar-movil nav a').on('click', mostrarTabs);
+
+    function mostrarTabs() {
+        $('.navbar-movil nav a').removeClass('active');
+        $(this).addClass('active');
+        var enlace = $(this).attr('href');
+        $('.main-body section').hide();
+        $(enlace).fadeIn(1000);
+        return false;
+    }
+});
+
+document.querySelector(".home-nav").addEventListener("click", (e) => {
+    navMovil.style.left = "-100%";
+})
+document.querySelector(".about-nav").addEventListener("click", (e) => {
+    navMovil.style.left = "-100%";
+})
+
+document.querySelector(".works-nav").addEventListener("click", (e) => {
+    navMovil.style.left = "-100%";
+})
+
+document.querySelector(".contact-nav").addEventListener("click", (e) => {
+    navMovil.style.left = "-100%";
+})
